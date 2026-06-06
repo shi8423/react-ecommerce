9398c7cf17955ce7da162b5e462ff4f1aa806031
shi8423 <shadowqueen.spy@gmail.com>
2026-06-06 12:55:41 +0530

    remove duplicate backend files

--- routes/authroutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { register, login } = require('../controllers/authController');
-
-let router = Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
